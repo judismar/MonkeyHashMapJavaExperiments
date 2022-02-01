06d8075ac2655b1dbb12bd448d5d2f50aa0bb297
judismar <solid.judis@gmail.com>
2022-02-01 11:49:53 -0300

    First commit

---
+++ util/MapImplementation.java
@@ -0,0 +1,43 @@
+package util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum MapImplementation {
+
+    HASH_MAP("HASH_MAP", false),
+    MONKEY_HASH_MAP("MONKEY", true),
+    CONCURRENT_HASH_MAP("CONCURRENT", true);
+
+    private static Map<String, MapImplementation> mapTypeByShortName = new HashMap<>();
+
+    static {
+        for (MapImplementation enumeration : MapImplementation.values()) {
+            mapTypeByShortName.put(enumeration.shortName, enumeration);
+        }
+    }
+
+    private String shortName;
+    private boolean threadSafe;
+
+    MapImplementation(String shortName, boolean threadSafe) {
+        this.shortName = shortName;
+        this.threadSafe = threadSafe;
+    }
+
+    public static MapImplementation getByShortName(String shortName) {
+        MapImplementation result = null;
+        if (shortName != null) {
+            result = mapTypeByShortName.get(shortName.toUpperCase());
+        }
+        return result != null ? result : HASH_MAP;
+    }
+
+    public boolean isThreadSafe() {
+        return threadSafe;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+}
